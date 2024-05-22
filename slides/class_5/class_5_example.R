b68a58a635a5757dfaa3a53ea8a38770f15523df
kkaput23 <kkaput23@gmail.com>
2024-05-22 11:58:23 -0500

    updates

---
+++ slides/class_5/class_5_example.R
@@ -0,0 +1,64 @@
+# class_5_example 
+# lasted updated 2024-05-22 by Krista Kaput
+
+# load -----
+
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(edbuildr)
+
+options(scipen = 999)
+
+# load in the Minnesota 2022 MCA Reading data 
+
+mn_mca_frpl_district_fy22_raw <- read_excel("slides/class_5/2022 MCA FRPL MN.xlsx", 
+                                    skip = 1)
+
+# load in the Minnesota free and reduced price (FRPL) count 
+
+mn_frpl_district_raw <- read_csv("slides/class_5/SchoolDistrictsFreeReducedPriceLunchEligibility2017to18.csv")
+
+# clean the MCA data ------
+
+mn_mca_frpl_district_fy22_clean <- mn_mca_frpl_district_fy22_raw |>
+  rename_with(tolower) |> # This makes all of the column names lower cased
+  filter(grade == 0) |>
+  rename(dist_number = "district number",
+         dist_type = "district type",
+         district = "district name",
+         total_tested = "total tested",
+         does_not_meet_count = "count level d",
+         partially_meets_count = "count level p",
+         meets_count = "count level m",
+         exceeds_count = "count level e") |>
+  select(dist_number, dist_type, district, total_tested, does_not_meet_count,
+         partially_meets_count, meets_count, exceeds_count)
+
+
+# Clean the district FRPL data ------
+
+mn_frpl_district_clean <- mn_frpl_district_raw |>
+  rename_with(tolower) |>
+  rename(dist_id = "districtnum",
+         district = schooldistrict,
+         total_enroll = totalstudents, 
+         free_lunch_count = freeelignum,
+         reduced_lunch_count = reduceelignum, 
+         frpl_count = totalelignum) |>
+  select(dist_id, district, total_enroll, free_lunch_count, reduced_lunch_count,
+         frpl_count)
+
+# Create the unique identifer -----
+
+# We know that we need 
+
+
+# Tidy workd space -----
+
+rm(mn_mca_frpl_district_fy22_raw, mn_frpl_district_raw)
+
+
+
+
+

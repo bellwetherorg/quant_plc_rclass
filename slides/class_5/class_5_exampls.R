552b2e96c931b26fbf3f3a1a4ebfc4eae27248f0
kkaput23 <kkaput23@gmail.com>
2024-05-22 11:43:00 -0500

    updates

---
+++ slides/class_5/class_5_exampls.R
@@ -0,0 +1,18 @@
+# class_5_example 
+# lasted updated 2024-05-22 by Krista Kaput
+
+# load -----
+
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(edbuildr)
+
+options(scipen = 999)
+
+# load in the Minnesota 2022 MCA Reading data 
+
+
+# load in the Minnesota free and reduced price (FRPL) count 
+
+# clean the data ------

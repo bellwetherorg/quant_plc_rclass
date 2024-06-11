7c9d4152823904a267edf1e702a33a969f15abbb
kkaput23 <kkaput23@gmail.com>
2024-06-11 08:03:14 -0500

    updates

---
+++ slides/class_5/wk-4-hw-review.R
@@ -0,0 +1,11 @@
+# class_4_left_join_hw 
+# lasted updated 2024-06-11 by Krista Kaput
+
+# load -----
+
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(edbuildr)
+
+options(scipen = 999)
